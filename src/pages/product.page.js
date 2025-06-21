66b75b937e2262e53532f4e599d20713f4364bae
pppoh <2ndPipatpong987123@gmail.com>
2025-06-21 21:24:17 +0700

    feat: Enhance ProductPage with sorting and cart functionalities
    - Added methods for sorting products: isProductsSortedA_Z, isProductsSortedZ_A, isProductsSortedLowToHigh, isProductsSortedHighToLow.
    - Implemented clickSortDropdown and selectSortOption methods for interacting with the sort dropdown.
    - Introduced ClickCartIcon method to navigate to the cart page and verify the URL.
    - Updated product.spec.js to include tests for sorting functionalities and cart navigation, with annotations for test cases.
    - Increased the number of actual workers in test-results.json for improved parallel execution.

--- src/pages/product.page.js
+++ src/pages/product.page.js
@@ -1,7 +1,6 @@
 import {Page} from '@playwright/test';
 import { LoginPage } from './login.page';
 import { validUser } from '../test-data/user';
-import { get } from 'http';
 
 export class ProductPage {
     /**
@@ -14,7 +13,14 @@ export class ProductPage {
 
     baseUrl = 'https://www.saucedemo.com/inventory.html';
 
+    containBaseUrlCartPage = 'https://www.saucedemo.com/inventory-item.html?id=';
+
     locatorAddToCartButtons = '.btn_small';
+    locatorSortDropdown = '.product_sort_container';
+    locatorNameProducts = '.inventory_item_name';;
+    locatorPriceProducts = '.inventory_item_price';
+    locatorCartIcon = '.inventory_item_img';
+    
 
     async gotoWithValidUser() {
         const loginPage = new LoginPage(this.page);
@@ -61,5 +67,97 @@ export class ProductPage {
             await this.clickAddToCartButton(button);
         }
     }
+
+    async clickSortDropdown() {
+        await this.page.locator(this.locatorSortDropdown).click();
+    }
+
+    async selectSortOption(option) {
+        await this.page.locator(this.locatorSortDropdown).selectOption(option);
+    }
+
+    // Sort A to Z 
+    async convertSnallLetterToNumber(letter) {
+        return letter.charCodeAt(0) - 96; // 'a' is 1, 'b' is 2, ..., 'z' is 26
+    }
+
+    async isProductsSortedA_Z() {
+        const products = await this.page.locator(this.locatorNameProducts).allTextContents();
+        const StartLetter = products.map(product => product.charAt(0).toLowerCase());
+        const NumberLetter_A =  await this.convertSnallLetterToNumber('a');
+        let lowestLetter = NumberLetter_A;
+
+        for (let letter of StartLetter) {
+            const currentLetterNumber = await this.convertSnallLetterToNumber(letter);
+            if (currentLetterNumber < lowestLetter) {
+                return false; 
+            }
+            lowestLetter = currentLetterNumber; 
+        }
+
+        return true;
+     }
+
+    async isProductsSortedZ_A() {
+        const products = await this.page.locator(this.locatorNameProducts).allTextContents();
+        const StartLetter = products.map(product => product.charAt(0).toLowerCase());
+        const NumberLetter_Z =  await this.convertSnallLetterToNumber('z');
+        let highestLetter = NumberLetter_Z;
+
+        for (let letter of StartLetter) {
+            const currentLetterNumber = await this.convertSnallLetterToNumber(letter);
+            if (currentLetterNumber > highestLetter) {
+                return false; 
+            }
+            highestLetter = currentLetterNumber; 
+        }
+
+        return true;
+     }
+
+    async isProductsSortedLowToHigh() {
+        const priceOfProducts = await this.page.locator(this.locatorPriceProducts).allTextContents();
+        const prices = priceOfProducts.map(price => parseFloat(price.replace('$', '')));
+        let lowestPrice = prices[0];
+        for (let price of prices) {
+            if (isNaN(price)) {
+                console.error(`Invalid price found: ${price}`);
+                return false; // If any price is not a number, return false
+            }
+            if (price < lowestPrice) {
+                console.error(`Price out of order: ${price} < ${lowestPrice}`);
+                return false; 
+            }
+            lowestPrice = price; 
+        }
+        
+        return true;
+    }
+
+    async isProductsSortedHighToLow() {
+        const priceOfProducts = await this.page.locator(this.locatorPriceProducts).allTextContents();
+        const prices = priceOfProducts.map(price => parseFloat(price.replace('$', '')));
+        let highestPrice = prices[0];
+        for (let price of prices) {
+            if (isNaN(price)) {
+                return false; // If any price is not a number, return false
+            }
+            if (price > highestPrice) {
+                return false; 
+            }
+            highestPrice = price; 
+        }
+        
+        return true;
+    }
+
+    async ClickCartIcon(indexCartIcon = 0) {
+        const cartIcons = this.page.locator(this.locatorCartIcon);
+        await cartIcons.nth(indexCartIcon).click();
+        
+        const currentUrl = await this.page.url();
+        return currentUrl.includes(this.containBaseUrlCartPage);
+}
+
     
 }
--- src/tests/product.spec.js
+++ src/tests/product.spec.js
@@ -1,12 +1,18 @@
 import {expect} from '@playwright/test';
 import {test} from '../pages/base';
+import { ADDRGETNETWORKPARAMS } from 'dns';
 
 test.beforeEach(async ({productPage}) => {
     await productPage.gotoWithValidUser();
 });
 
-test.only('Adding all available products to the cart and then removing them, verifying that the cart updates correctly', async ({productPage}) => {
-   await productPage.clickAllAddToCartButtons();
+test('Adding all available products to the cart and then removing them, verifying that the cart updates correctly', async ({productPage}) => {
+   test.info().annotations.push({
+         type: 'testId',
+         description: 'TC-7'
+    });
+
+    await productPage.clickAllAddToCartButtons();
    expect( await productPage.isTextFromAllAddToCartButton('Remove')).toBe(true);
    //click all "Remove" buttons
     await productPage.clickAllAddToCartButtons();
@@ -15,18 +21,56 @@ test.only('Adding all available products to the cart and then removing them, ver
 });
 
 test('Product should correctly sorts items from A to Z', async ({productPage}) => {
+    test.info().annotations.push({
+        type: 'testId',
+        description: 'TC-8'
+    });
+
+    await productPage.clickSortDropdown();
+    await productPage.selectSortOption('az');
+    expect(await productPage.isProductsSortedA_Z()).toBe(true);
+
 });
 
 test('Product should correctly sorts items from Z to A', async ({productPage}) => {
+    test.info().annotations.push({
+        type: 'testId',
+        description: 'TC-9'
+    });
+
+    await productPage.clickSortDropdown();
+    await productPage.selectSortOption('za');
+    expect(await productPage.isProductsSortedZ_A()).toBe(true);
 });
 
 test('Product should correctly sorts items from low to high', async ({productPage}) => {
+    test.info().annotations.push({
+        type: 'testId',
+        description: 'TC-10'
+    });
+
+    await productPage.clickSortDropdown();
+    await productPage.selectSortOption('lohi');
+    expect(await productPage.isProductsSortedLowToHigh()).toBe(true);
 });
 
 test('Product should correctly sorts items from high to low', async ({productPage}) => {
+    test.info().annotations.push({
+        type: 'testId',
+        description: 'TC-11'
+    });
+
+    await productPage.clickSortDropdown();
+    await productPage.selectSortOption('hilo');
+    expect(await productPage.isProductsSortedHighToLow()).toBe(true);
 });
 
 test('Should navigate to the cart page when clicking the cart icon', async ({productPage}) => {
+    test.info().annotations.push({
+        type: 'testId',
+        description: 'TC-12'
+    });
 
+    expect(await productPage.ClickCartIcon(2)).toBe(true);
 });
 
